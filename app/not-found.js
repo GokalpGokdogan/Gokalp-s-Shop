2ac6e4056d1f42a9eeb069f130efc2c8e518a6ca
Gokalp Gokdogan <gokalp.gokdogan@gmail.com>
2025-09-17 23:24:01 +0300

    Small fix

--- app/not-found.js
+++ app/not-found.js
@@ -4,7 +4,7 @@ export default function NotFound() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 text-center p-6">
       <h1 className="text-4xl font-bold text-gray-900 mb-4">404</h1>
       <p className="text-gray-600 mb-6">
-        Sorry, the page you are looking for doesn't exist or has been removed.
+        Sorry, the page you are looking for does not exist or has been removed.
       </p>
       <Link
         href="/"
